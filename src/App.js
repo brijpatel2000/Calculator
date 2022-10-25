822224fbad37bf558e390989e935a7f6f546e7b0
brijesh tilava <tilavabrijesh786@gmail.com>
2022-10-25 16:46:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import './style.scss';
+import Calculator from './MyComponent/Calculator';
+
+function App() {
+  
+  return (
+    <Calculator/>
+  );
+}
+
+export default App;
